61681510a8cd4e2ce606ab85c21323b40fb307c8
Konstantin <slymask3@protonmail.com>
2022-08-31 01:53:08 -0400

    cleanup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,14 @@
 
 ## Unreleased
 ### Added
-- Initial release
+- Split off from [Instant Blocks v1.6.5](https://github.com/Slymask3/instant-blocks/tree/1.19.2-1.6.5)
+
+### Removed
+- Instant wooden house
+- Instant glass dome
+- Instant farm
+- Instant skydive
+- Instant pool
+- Instant statue
+- Instant schematic
+- Instant huge tree
--- common/src/main/java/com/slymask3/instantblocks/utility/init/Registration.java
+++ common/src/main/java/com/slymask3/instantblocks/utility/init/Registration.java
@@ -8,47 +8,48 @@ import com.slymask3.instantblocks.utility.registry.ModTiles;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class Registration {
     public static final class Names {
-        public static final String IB_MINING_LADDER = "instant_mining_ladder";
-        public static final String IB_GRINDER = "instant_grinder";
-        public static final String IB_ESCAPE_LADDER = "instant_escape_ladder";
-        public static final String IB_WATER = "instant_water";
-        public static final String IB_LAVA = "instant_lava";
-        public static final String IB_SUCTION = "instant_suction";
-        public static final String IB_HARVEST = "instant_harvest";
-        public static final String IB_LIGHT = "instant_light";
+        public static final String MINING_LADDER = "instant_mining_ladder";
+        public static final String GRINDER = "instant_grinder";
+        public static final String ESCAPE_LADDER = "instant_escape_ladder";
+        public static final String WATER = "instant_water";
+        public static final String LAVA = "instant_lava";
+        public static final String SUCTION = "instant_suction";
+        public static final String HARVEST = "instant_harvest";
+        public static final String LIGHT = "instant_light";
     }
     
     public static void registerBlocks(IRegistryHelper<Block> helper) {
         ModBlocks.init();
-        helper.register(resource(Names.IB_MINING_LADDER), ModBlocks.INSTANT_MINING_LADDER);
-        helper.register(resource(Names.IB_GRINDER), ModBlocks.INSTANT_GRINDER);
-        helper.register(resource(Names.IB_ESCAPE_LADDER), ModBlocks.INSTANT_ESCAPE_LADDER);
-        helper.register(resource(Names.IB_WATER), ModBlocks.INSTANT_WATER);
-        helper.register(resource(Names.IB_LAVA), ModBlocks.INSTANT_LAVA);
-        helper.register(resource(Names.IB_SUCTION), ModBlocks.INSTANT_SUCTION);
-        helper.register(resource(Names.IB_HARVEST), ModBlocks.INSTANT_HARVEST);
-        helper.register(resource(Names.IB_LIGHT), ModBlocks.INSTANT_LIGHT);
+        helper.register(resource(Names.MINING_LADDER), ModBlocks.INSTANT_MINING_LADDER);
+        helper.register(resource(Names.GRINDER), ModBlocks.INSTANT_GRINDER);
+        helper.register(resource(Names.ESCAPE_LADDER), ModBlocks.INSTANT_ESCAPE_LADDER);
+        helper.register(resource(Names.WATER), ModBlocks.INSTANT_WATER);
+        helper.register(resource(Names.LAVA), ModBlocks.INSTANT_LAVA);
+        helper.register(resource(Names.SUCTION), ModBlocks.INSTANT_SUCTION);
+        helper.register(resource(Names.HARVEST), ModBlocks.INSTANT_HARVEST);
+        helper.register(resource(Names.LIGHT), ModBlocks.INSTANT_LIGHT);
     }
 
     public static void registerItems(IRegistryHelper<Item> helper) {
-        helper.register(resource(Names.IB_MINING_LADDER), blockItem(ModBlocks.INSTANT_MINING_LADDER));
-        helper.register(resource(Names.IB_GRINDER), blockItem(ModBlocks.INSTANT_GRINDER));
-        helper.register(resource(Names.IB_ESCAPE_LADDER), blockItem(ModBlocks.INSTANT_ESCAPE_LADDER));
-        helper.register(resource(Names.IB_WATER), blockItem(ModBlocks.INSTANT_WATER));
-        helper.register(resource(Names.IB_LAVA), blockItem(ModBlocks.INSTANT_LAVA));
-        helper.register(resource(Names.IB_SUCTION), blockItem(ModBlocks.INSTANT_SUCTION));
-        helper.register(resource(Names.IB_HARVEST), blockItem(ModBlocks.INSTANT_HARVEST));
-        helper.register(resource(Names.IB_LIGHT), blockItem(ModBlocks.INSTANT_LIGHT));
+        helper.register(resource(Names.MINING_LADDER), blockItem(ModBlocks.INSTANT_MINING_LADDER));
+        helper.register(resource(Names.GRINDER), blockItem(ModBlocks.INSTANT_GRINDER));
+        helper.register(resource(Names.ESCAPE_LADDER), blockItem(ModBlocks.INSTANT_ESCAPE_LADDER));
+        helper.register(resource(Names.WATER), blockItem(ModBlocks.INSTANT_WATER));
+        helper.register(resource(Names.LAVA), blockItem(ModBlocks.INSTANT_LAVA));
+        helper.register(resource(Names.SUCTION), blockItem(ModBlocks.INSTANT_SUCTION));
+        helper.register(resource(Names.HARVEST), blockItem(ModBlocks.INSTANT_HARVEST));
+        helper.register(resource(Names.LIGHT), blockItem(ModBlocks.INSTANT_LIGHT));
     }
 
     public static void registerTiles(IRegistryHelper<BlockEntityType<?>> helper) {
         Common.TILES.init();
-        helper.register(resource(Names.IB_HARVEST), ModTiles.HARVEST);
+        helper.register(resource(Names.HARVEST), ModTiles.HARVEST);
     }
 
     private static ResourceLocation resource(String name) {
@@ -57,6 +58,6 @@ public class Registration {
     
     private static BlockItem blockItem(Block block) {
         Common.LOG.info("Core.ITEM_GROUP: {}", Core.ITEM_GROUP);
-        return new BlockItem(block,new Item.Properties().tab(Core.ITEM_GROUP));
+        return new BlockItem(block,new Item.Properties().tab(Core.ITEM_GROUP).rarity(Rarity.RARE));
     }
 }
